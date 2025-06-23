281b5d257205983f18cd510f81a1cb706772029f
Taylor Turner <bonee94@yahoo.com>
2025-06-22 20:07:28 -0500

    created serviceType and customer models

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,9 @@
+public class Customer
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
+    public required string ServiceType { get; set; }
+    public required string Notes { get; set; }
+}
---
+++ Models/ServiceTypeModel.cs
@@ -0,0 +1,5 @@
+public class ServiceType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+};
