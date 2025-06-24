0bbd313ee82e72ca8aac7fe23d94cbab27fbcbfa
Taylor Turner <bonee94@yahoo.com>
2025-06-23 19:01:49 -0500

    added custom validator to customer model to check that at least one new job has been added to customer

--- Models/CustomerModel.cs
+++ Models/CustomerModel.cs
@@ -1,9 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Customer
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-    public required string Phone { get; set; }
 
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "Email is required")]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Phone is required")]
+    public string Phone { get; set; } = "";
+
+    [RequireAtLeastOneJob]
     public List<Job> Jobs { get; set; } = new();
+}
+
+
+public class RequireAtLeastOneJobAttribute : ValidationAttribute
+{
+    public RequireAtLeastOneJobAttribute()
+    {
+        ErrorMessage = "A Service Type and Note are required.";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is List<Job> jobList)
+        {
+            if (jobList.Count > 0)
+            {
+                return ValidationResult.Success;
+            }
+        }
+
+        return new ValidationResult(ErrorMessage);
+    }
 }
